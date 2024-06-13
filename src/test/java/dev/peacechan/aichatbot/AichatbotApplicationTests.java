1281b86f5bb3aa3213941737b8ca53d18d11aa07
AyeChanHtunNaing <ayechanhtunnaing@icloud.com>
2024-06-13 22:30:31 +0630

    init

---
+++ src/test/java/dev/peacechan/aichatbot/AichatbotApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.peacechan.aichatbot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AichatbotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
